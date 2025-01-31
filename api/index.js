89d76380ea0bce14ea1ed97e54c9a876b6776291
vinod <21r21a3344@mlrinstitutions.ac.in>
2025-01-31 20:29:38 +0530

    changes for vercel

--- api/index.js
+++ api/index.js
@@ -26,8 +26,9 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}!`);
 });
 
 app.use('/api/user', userRoutes);
